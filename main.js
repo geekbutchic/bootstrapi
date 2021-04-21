70a394f389f2b5998159d0084f435dea68045723
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2021-04-20 23:45:02 -0400

    updates to fetch(URL)

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-const xhr = new XMLHttpRequest();
+const URL = "https://dog.ceo/api/breeds/image/random";
 const button = document.querySelector("#button");
 const img = document.querySelector("img");
 
-xhr.addEventListener("loadend", () => {
-  const data = JSON.parse(xhr.responseText);
-  img.src = data.message;
-});
-
 button.addEventListener("click", () => {
-  xhr.open("GET", "https://dog.ceo/api/breeds/image/random");
-  xhr.setRequestHeader("Accept", "application/json");
-  xhr.send();
+  fetch(URL)
+    .then((res) => res.json())
+    .then((data) => {
+      img.src = data.message;
+    });
 });
